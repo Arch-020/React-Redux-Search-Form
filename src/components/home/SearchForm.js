bbb1f59cb4965614cc0070789b3685dcf8bf6009
Arch-020 <archana.gawari@fafadiatech.com>
2022-09-05 12:27:50 +0530

    minor change

--- src/components/home/SearchForm.js
+++ src/components/home/SearchForm.js
@@ -17,7 +17,7 @@ export class SearchForm extends Component {
 
   render() {
     return (
-      <div className="jumbotron jumbotron-fluid nt-5 text-center">
+      <div className="jumbotron jumbotron-fluid mt-5 text-center">
           <div className="container">
               <h1 className="display-4 mb-3">
                 Search for a news article ..
